1ed1009eb25262c503faf3d0a4447630fc0a7847
Victor Tang <tanghengzhi@live.com>
2023-11-27 16:55:19 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Netboom Docs",
+  title: "Cloud Play Docs",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
